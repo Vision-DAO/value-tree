2f78f90033d4b2279d1db5a394ca5c51ca4ca679
Dowland Aiello <dowlandaiello@gmail.com>
2022-08-29 22:41:31 -0400

    Deploy to registry in migrations.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,8 +5,14 @@ const ROOT_IDEA = require("./conf");
 
 const ipfs = require("ipfs-core");
 
-module.exports = function (deployer) {
-	deployer.deploy(Factory);
-	deployer.deploy(Dao, ROOT_IDEA.name, ROOT_IDEA.ticker, ROOT_IDEA.shares, ROOT_IDEA.detailsIpfsID);
-	deployer.deploy(Prop, ...ROOT_IDEA.propArgs);
+module.exports = async function (deployer) {
+	await deployer.deploy(Factory);
+	const registry = await Factory.deployed();
+
+	await deployer.deploy(Dao, ROOT_IDEA.name, ROOT_IDEA.ticker, ROOT_IDEA.shares, ROOT_IDEA.detailsIpfsID);
+
+	// Register the new DAO as a DAO under the registry
+	await registry.createIdea(ROOT_IDEA.name, ROOT_IDEA.ticker, ROOT_IDEA.shares, ROOT_IDEA.detailsIpfsID);
+
+	await deployer.deploy(Prop, ...ROOT_IDEA.propArgs);
 };
